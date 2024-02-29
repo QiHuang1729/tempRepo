2df7441a31e7012913cfe1b9138fa9db68b9b371
QiHuang1729 <130502220+QiHuang1729@users.noreply.github.com>
2024-02-29 11:36:24 -0800

    Add files via upload

--- SimpleCalc/SimpleCalc.java
+++ SimpleCalc/SimpleCalc.java
@@ -1,5 +1,12 @@
 import java.util.List;		// used by expression evaluator
 
+
+// don't focus on minutae like this. You can spend 10 min
+				// debating on which design is the cleanest using all the 
+				// stylistic explanations, rationales, reasons, etc. your
+				// various teachers has shown to you, and absolutely no one
+				// will pay attention to it. 
+
 /**
  *	This class outputs the result of correctly entered arithmetic 
  * 	expressions.
@@ -39,8 +46,12 @@ public class SimpleCalc {
 		List<String> tokens = null;
 		while (!input.equals("q")) {
 			input = Prompt.getString("");
-			tokens = tokenizeExpression(input);
-			evaluateExpression(tokens);
+			if (input.equals("h")) {
+				printHelp();
+			} else {
+				tokens = tokenizeExpression(input);
+				evaluateExpression(tokens);
+			}
 		}
 	}
 	
@@ -61,12 +72,64 @@ public class SimpleCalc {
 	 */
 	public double evaluateExpression(List<String> tokens) {
 		double value = 0;
-		String theToken = tokens.get(i);
+		String theToken = "";
 		for (int i = 0; i < tokens.length; i++) {
-			if ('0' < theToken.charAt(0) < '9') {
+			theToken = tokens.get(i);
+			/* if you have a number, just push the number into the stack
+			 * for this algorithm */
+			if ('0' < theToken.charAt(0) && theToken.charAt(0) < '9') {
 				valueStack.push(Double.parseDouble(theToken));
 			} else {
-				if (theToken.equals(
+				/* if you have a + or - and you see a + or -, evaluate the
+				 * previous + or -. If you see anything else of a higher
+				 * precedence, also evaluate it. If its an exponent, you 
+				 * should continue checking until you don't have one anymore, 
+				 * then evaluate whatever is left. Then add your + or - to the stack
+				 * if you have a * or / and you see a - or +, you can put your 
+				 * operation in the stack. If you see something of a higher
+				 * precedence, evaluate it. If it's an exponent, keep 
+				 * going to the left, since its right-associative. 
+				 * If the precedence is lower, you can move on to the 
+				 * next operation.
+				 * if you have a ) after any operation, do the operation
+				 * you saw
+				 * continue doing operations until you find a (, then stop.
+				 * if you see a + after a /, do the / first. 
+				 * if you have a (, just continue along, since you can't
+				 * do anything
+				 * if you have a ^, always move on since you don't know 
+				 * if the next operation is an ^. Then you will hit the 
+				 * end, a right parenthesis, or a lower precedence 
+				 * operation, in which case you should just evaluate it.
+				 */
+				 
+				String prevOperator = operatorStack.peek();
+				if (hasPrecedence(theToken, prevOperator)) {
+					double num2 = valueStack.pop();
+					double num1 = valueStack.pop();
+					operatorStack.pop();
+					if (prevOperator.equals("+")) {
+						valueStack.add(num2 + num1);
+					} else if (prevOperator.equals("-") {
+						valueStack.add(num2 - num1);
+					} else if (prevOperator.equals("*")) {
+						valueStack.add(num2 * num1);
+					} else if (prevOperator.equals("/")) {
+						valueStack.add(num2 / num1);
+					} else if (prevOperator.equals("^") {
+						// could be cleaner?
+						while (prevOperator.equals("^")) {
+							num2 = Math.pow(num1, num2);
+							prevOperator = operatorStack.pop();
+							if (prevOperator.equals("^")) {
+								num1 = valueStack.pop();
+							}
+						}
+						valueStack.add(num2);
+					}
+				} else {
+					
+				}
 			}
 		}
 		
@@ -93,5 +156,8 @@ public class SimpleCalc {
 			return false;
 		return true;
 	}
-	 
+	
+	// it should work. Maybe you are using the operators backwards or 
+	// interpreting it wrong (interpreting the expression or individual
+	// operation wrong)
 }
